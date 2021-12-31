c55d786b41f8caa862de6387ac8d890221a28300
James Teng <jamest.1402@gmail.com>
2021-12-31 16:28:21 +1100

    ajfpojefpwsojkfg

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import fragment from "./assets/fragment.glsl";
 import vertex from "./assets/vertexParticles.glsl";
+import { PerspectiveCamera } from 'three';
 
 // ******************************************************** SCENE SETUP ******************************************************************************
 const scene = new THREE.Scene(); // container for threejs objects
@@ -11,11 +12,12 @@ scene.background = new THREE.Color( 0x1e1e25 );
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); // fov, aspect ratio, view frustrum
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg')
+  canvas: document.querySelector('#bg'),
+  powerPreference: "high-performance"
 })
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight); 
 camera.position.setZ(40);
 
 const pointLight = new THREE.PointLight(0xffffff, 2);
@@ -28,7 +30,7 @@ scene.add(ambientLight, pointLight);
 // ********************************************************* BITCOIN ******************************************************************************
 const bitcoinColour = new THREE.TextureLoader().load('./bitcoin.png');
 
-const bitcoinGeometry = new THREE.CylinderGeometry( 4, 4, 0.5, 64 );
+const bitcoinGeometry = new THREE.CylinderBufferGeometry( 4, 4, 0.5, 64 );
 const bitcoinMaterial = new THREE.MeshStandardMaterial({
     map: bitcoinColour,
     metalness:0.7,
@@ -49,7 +51,7 @@ scene.add( bitcoin );
 // ********************************************************* ETH ******************************************************************************
 const ethColour = new THREE.TextureLoader().load('./eth.png');
 
-const ethGeometry = new THREE.CylinderGeometry( 4, 4  , 0.5, 64 );
+const ethGeometry = new THREE.CylinderBufferGeometry( 4, 4  , 0.5, 64 );
 const ethMaterial = new THREE.MeshStandardMaterial({
     map: ethColour,
     metalness:0.7,
@@ -88,7 +90,7 @@ loader.load('./dna.glb',
         uColor3: {value: new THREE.Color(0x8D806F)},
         resolution: {value: new THREE.Vector4()}
       },
-      transparent:true,
+      // transparent:true,
       vertexShader: vertex,
       fragmentShader: fragment,
       depthTest: false,
@@ -96,6 +98,7 @@ loader.load('./dna.glb',
       blending: THREE.AdditiveBlending
     });
     const pixelNum = geometry.attributes.position.array.length/3;
+    console.log(pixelNum);
     let randomSize = new Float32Array(pixelNum);
     let randomColour = new Float32Array(pixelNum);
 
@@ -114,21 +117,58 @@ loader.load('./dna.glb',
 		scene.add( dnaShape );
 
     // ******************************************************** ANIMATION LOOP *****************************
-
-
     function animate() {
-        requestAnimationFrame(animate);
+      requestAnimationFrame(animate);
 
-        ethCoin.rotation.x -= 0.00009;
-        ethCoin.rotation.y -= 0.00009;
+      ethCoin.rotation.x -= 0.00009;
+      ethCoin.rotation.y -= 0.00009;
 
-        bitcoin.rotation.x += 0.0001;
-        bitcoin.rotation.y += 0.0001;
+      bitcoin.rotation.x += 0.0001;
+      bitcoin.rotation.y += 0.0001;
 
-        dnaShape.rotation.y += 0.00006;
+      dnaShape.rotation.y += 0.00006;
 
-        renderer.render(scene, camera);
+      renderer.render(scene, camera);
     }
       
     animate()
 })
+
+
+document.body.onscroll = function(e) {
+  const t = document.body.getBoundingClientRect().top; // get scroll position 
+  // if scrolling up
+  if (this.oldScroll > this.scrollY) {
+    ethCoin.rotation.x += 0.009;
+    ethCoin.rotation.y += 0.009;
+  
+    bitcoin.rotation.x -= 0.009;
+    bitcoin.rotation.y -= 0.009;
+  
+    dnaShape.rotation.y -= 0.01;
+  }
+  // if scrolling down
+
+  else {
+    ethCoin.rotation.x -= 0.009;
+    ethCoin.rotation.y -= 0.009;
+  
+    bitcoin.rotation.x += 0.009;
+    bitcoin.rotation.y += 0.009;
+  
+    dnaShape.rotation.y += 0.01;
+  }
+  // camera.position.z = t * -0.01;
+  // camera.position.x = t * -0.0002;
+  this.oldScroll = this.scrollY;
+  dnaShape.updateMatrix()
+}
+
+function scrollHandler() {
+  document.querySelector(".scroll-label").style.opacity = "0"; 
+  document.querySelector(".scroll-indicator").style.height = "0"; 
+  setTimeout(() => { document.querySelector(".scroll-container").remove();}, 2000);
+  document.removeEventListener('scroll', scrollHandler);
+}
+
+document.addEventListener("scroll", scrollHandler);
